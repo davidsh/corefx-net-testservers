026e6a924b5d2a02baf122904cd25a24e97c7cd1
David Shulman <david.shulman@microsoft.com>
2016-01-10 20:05:50 -0800

    Add more endpoints

---
+++ CoreFxNetCloudService/WebServer/EmptyContent.ashx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServer
+{
+    /// <summary>
+    /// Summary description for EmptyContent
+    /// </summary>
+    public class EmptyContent : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            // By default, this empty method sends back a 200 status code with 'Content-Length: 0' response header.
+            // There are no other entity-body related (i.e. 'Content-Type') headers returned.
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ CoreFxNetCloudService/WebServer/Redirect.ashx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Web;
+
+namespace WebServer
+{
+    /// <summary>
+    /// Summary description for Redirect
+    /// </summary>
+    public class Redirect : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            string redirectUri = context.Request.QueryString["uri"];
+            string hopsString = context.Request.QueryString["hops"];
+            int hops = 1;
+
+            RequestHelper.AddResponseCookies(context);
+
+            try
+            {
+                if (string.IsNullOrEmpty(redirectUri))
+                {
+                    context.Response.StatusCode = 500;
+                    context.Response.StatusDescription = "Missing redirection uri";
+                    return;
+                }
+
+                if (!string.IsNullOrEmpty(hopsString))
+                {
+                    hops = int.Parse(hopsString);
+                }
+
+
+                if (hops <= 1)
+                {
+                    context.Response.Headers.Add("Location", redirectUri);
+                }
+                else
+                {
+                    context.Response.Headers.Add(
+                        "Location",
+                        string.Format("/Redirect.ashx?uri={0}&hops={1}",
+                        redirectUri,
+                        hops - 1));
+                }
+
+                context.Response.StatusCode = 302;
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = 500;
+                context.Response.StatusDescription = "Error parsing hops: " + hopsString;
+            }
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
